f306aba93f9a55ee969f613e937829491a4d84b9
Olena-Rudnieva <rudnievao@gmail.com>
2023-08-21 19:54:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -19,22 +19,22 @@ function updateControlActiveWidth() {
   controlActive.style.width = `${controlWidth}%`;
 }
 
-sliderContainer.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  startPositionX = e.clientX;
-});
+// sliderContainer.addEventListener('mousedown', (e) => {
+//   isDragging = true;
+//   startPositionX = e.clientX;
+// });
 
 sliderContainer.addEventListener('touchstart', (e) => {
   isDragging = true;
   startPositionX = e.clientX;
 });
 
-sliderContainer.addEventListener('mousemove', (e) => {
-  if (!isDragging) return;
-  const moveX = e.clientX - startPositionX;
-  currentTranslate = -currentIndex * slideWidth + moveX;
-  updateSliderPosition();
-});
+// sliderContainer.addEventListener('mousemove', (e) => {
+//   if (!isDragging) return;
+//   const moveX = e.clientX - startPositionX;
+//   currentTranslate = -currentIndex * slideWidth + moveX;
+//   updateSliderPosition();
+// });
 
 sliderContainer.addEventListener('touchmove', (e) => {
   if (!isDragging) return;
@@ -64,22 +64,22 @@ sliderContainer.addEventListener('touchmove', (e) => {
 //   updateControlActiveWidth();
 // });
 
-sliderContainer.addEventListener('mouseup', () => {
-  isDragging = false;
-  currentIndex = (currentIndex + 1) % slideCount;
-  currentTranslate = -currentIndex * slideWidth;
-  updateSliderPosition();
-  updateControlActiveWidth();
-  //   const movedBy = -currentTranslate / slides[0].offsetWidth;
-  //   if (movedBy < -0.5) {
-  //     currentIndex = Math.min(currentIndex + 1, slideCount - 1);
-  //   } else if (movedBy > 0.5) {
-  //     currentIndex = Math.max(currentIndex - 1, 0);
-  //   }
-  //   currentTranslate = -currentIndex * slides[0].offsetWidth;
-  //   updateSliderPosition();
-  //   updateControlActiveWidth();
-});
+// sliderContainer.addEventListener('mouseup', () => {
+//   isDragging = false;
+//   currentIndex = (currentIndex + 1) % slideCount;
+//   currentTranslate = -currentIndex * slideWidth;
+//   updateSliderPosition();
+//   updateControlActiveWidth();
+//   //   const movedBy = -currentTranslate / slides[0].offsetWidth;
+//   //   if (movedBy < -0.5) {
+//   //     currentIndex = Math.min(currentIndex + 1, slideCount - 1);
+//   //   } else if (movedBy > 0.5) {
+//   //     currentIndex = Math.max(currentIndex - 1, 0);
+//   //   }
+//   //   currentTranslate = -currentIndex * slides[0].offsetWidth;
+//   //   updateSliderPosition();
+//   //   updateControlActiveWidth();
+// });
 
 sliderContainer.addEventListener('touchend', () => {
   isDragging = false;
