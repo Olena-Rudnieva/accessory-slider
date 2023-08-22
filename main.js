d2c143a8cd5be4f3336458f152218a73eb076d5c
Olena-Rudnieva <rudnievao@gmail.com>
2023-08-22 14:35:26 +0300

    Add screen differences

--- main.js
+++ main.js
@@ -3,92 +3,46 @@ const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const controlActive = document.querySelector('.control-active');
 
-// let currentIndex = 0;
-// const slideCount = slides.length;
-// let isDragging = false;
-// let startPositionX = 0;
-// let currentTranslate = 0;
-// const slideWidth = slides[0].offsetWidth;
-
-// function updateSliderPosition() {
-//   slider.style.transform = `translateX(${currentTranslate}px)`;
-// }
-
-// function updateControlActiveWidth() {
-//   const controlWidth = (100 / slideCount) * (currentIndex + 1);
-//   controlActive.style.width = `${controlWidth}%`;
-// }
-
-// sliderContainer.addEventListener('mousedown', (e) => {
-//   isDragging = true;
-//   startPositionX = e.clientX;
-// });
-
-// sliderContainer.addEventListener('mousemove', (e) => {
-//   if (!isDragging) return;
-//   const moveX = e.clientX - startPositionX;
-//   currentTranslate = -currentIndex * slideWidth + moveX;
-
-//   updateSliderPosition();
-// });
-
-// sliderContainer.addEventListener('mouseup', () => {
-//   isDragging = false;
-//   currentIndex = (currentIndex + 1) % slideCount;
-//   currentTranslate = -currentIndex * slideWidth;
-//   updateSliderPosition();
-//   updateControlActiveWidth();
-// });
-
-// sliderContainer.addEventListener('mouseleave', () => {
-//   isDragging = false;
-//   currentIndex = (currentIndex + 1) % slideCount;
-//   currentTranslate = -currentIndex * slideWidth;
-//   updateSliderPosition();
-//   updateControlActiveWidth();
-// });
-
-// sliderContainer.addEventListener('mouseup', () => {
-//   isDragging = false;
-//   const movedBy = -currentTranslate / slideWidth;
-//   console.log(movedBy);
-//   if (movedBy < -0.5) {
-//     currentIndex = Math.min(currentIndex + 1, slideCount - 1);
-//   } else if (movedBy > 0.5) {
-//     currentIndex = Math.max(currentIndex - 1, 0);
-//   }
-//   currentTranslate = -currentIndex * slideWidth;
-//   updateSliderPosition();
-//   updateControlActiveWidth();
-// });
-
-// setInterval(() => {
-//   currentIndex = (currentIndex + 1) % slideCount;
-//   currentTranslate = -currentIndex * slides[0].offsetWidth;
-//   updateSliderPosition();
-//   updateControlActiveWidth();
-// }, 3000);
-
+let currentIndex = 0;
+const slideCount = slides.length;
 let isDragging = false;
-let startPosX = 0;
+let startPositionX = 0;
 let currentTranslate = 0;
-let prevTranslate = 0;
+const slideWidth = slides[0].offsetWidth;
 
-slider.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  startPosX = e.clientX;
-  prevTranslate = currentTranslate;
-});
+function updateSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+}
 
-slider.addEventListener('mousemove', (e) => {
-  if (isDragging) {
-    const distanceX = e.clientX - startPosX;
-    console.log(distanceX);
-    currentTranslate = prevTranslate + distanceX;
-    slider.style.transform = `translateX(${currentTranslate}px)`;
+function updateControlActiveWidth() {
+  const controlWidth = (100 / slideCount) * (currentIndex + 1);
+  controlActive.style.width = `${controlWidth}%`;
+}
+
+function checkScreenWidth() {
+  if (window.innerWidth < 768) {
+    sliderContainer.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startPositionX = e.clientX;
+    });
+
+    sliderContainer.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      const moveX = e.clientX - startPositionX;
+      currentTranslate = -currentIndex * slideWidth + moveX;
+      updateSliderPosition();
+    });
+
+    sliderContainer.addEventListener('mouseup', () => {
+      isDragging = false;
+      // currentIndex = (currentIndex + 1) % slideCount;
+      // currentTranslate = -currentIndex * slideWidth;
+      // updateSliderPosition();
+      // updateControlActiveWidth();
+      console.log('stop');
+    });
   }
-});
+}
 
-slider.addEventListener('mouseup', () => {
-  isDragging = false;
-});
+window.onload = checkScreenWidth;
+window.addEventListener('resize', checkScreenWidth);
